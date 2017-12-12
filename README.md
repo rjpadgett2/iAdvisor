d14cf09a96ca92beb2450207a4a9ab261e9630ca
Reggie Padgett <reggie.padgett2@gmail.com>
2017-12-12 14:14:08 -0500

    Added all pages. Still working on connecting everything. Pages could use styling since they are very plain still.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # iAdvisor-
 Online advising tool developed for INST377
+
+This tool will automatically let students create a 4 year plan and prevent them
+from make simple scheduling errors.
+
+login_screen.html:
+Simple login screen that uses google authentication api. Once the use signs in
+a menu pops up either allowing the user to make and edit thier current 4 year plan builder
+or directs them to a survey for new users that allows them to fill out a form gauging
+how many INST classes they have taken thus far.
+
+user_info_form.html:
+Simple form getting the users already taken INST classes
+
+plan_builder.html:
+Allows user to create and store 4 year plan_builder
+
+user_results.php:
+Connects to INST database and takes in user input.
--- user_results.php
+++ user_results.php
@@ -3,7 +3,7 @@
 $server = "localhost";
 $username = "root";
 $password = "root";
-$db = "sakila";
+$db = "iAdvisor";
 
 // Create connection
 $conn = mysqli_connect($server, $username, $password, $db);
@@ -13,6 +13,20 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully<br><br>";
+
+$newUserResults = $_POST["checkbox"];
+$firstName = $_POST["firstname"];
+$lastName = $_POST["lastname"];
+
+/*
+if (isset($newUserResults)){
+	// $animal = stripslashes($animal);
+	$animal = str_replace(' ', '', $animal);
+	echo "Received <strong>" . $animal . "</strong> from the form.";
+} else {
+	echo "Nothing was received...";
+}
+*/
 
 // A function for general queries.
 function query_to_db($conn, $sql){
@@ -27,11 +41,6 @@ function query_to_db($conn, $sql){
 }
 
 
-// Receving the submitted data
-$age = $_POST["age"];
-$freq = $_POST["freq"];
-
-
 echo "Submitted Data: <br>";
 echo "Age? " . $age . "<br>";
 
@@ -43,10 +52,6 @@ $answer1 = $age > 21 ? "yes" : "no";
 
 
 // // You need to save the data into the database.
-$sql = "INSERT INTO alcohol (adult, alcohol_freq) VALUES ('$answer1', '$freq');";
-query_to_db($conn, $sql);
-
-
 
 
 mysqli_close($conn);
