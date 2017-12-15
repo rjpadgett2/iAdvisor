3eb96153042743a2a20e1edcb63cd9d2d7bd8b56
Reggie Padgett <reggie.padgett2@gmail.com>
2017-12-15 06:51:09 -0500

    Updated form_check

--- form_check.php
+++ form_check.php
@@ -1,4 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Droppable - Revert draggable position</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
+    <link href="jquery-ui-1.12.1.custom/jquery-ui.css" rel="stylesheet">
+    <link href="jquery-ui-1.12.1.custom/jquery-ui.theme.css" rel="stylesheet">
+    <script src="jquery-ui-1.12.1.custom/jquery-ui.js"></script>
 
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <style>
+  div:not(:last-child) {
+  margin-right: 500px;
+  overflow:auto;
+  }
+  #sortable1, #sortable2 {
+   border: 1px solid #eee;
+   width: 300px;
+   min-height: 20px;
+   list-style-type: none;
+   margin: 0;
+   padding: 5px 0 0 0;
+   float: left;
+   margin-right: 10px;
+ }
+ #sortable1 li, #sortable2 li {
+   margin: 0 5px 5px 5px;
+   padding: 10px;
+   font-size: 1.2em;
+   width: 300px;
+ }
+  .draggable {
+    width: 100px;
+    height: 100px;
+    padding: 0.5em;
+    float: left;
+    margin: 10px 10px 10px 0;
+  }
+  #droppable {
+    width: 150px;
+    min-height: 150px;
+    padding: 0.5em;
+    /*float: left; */
+    margin: 10px;
+  }
+  #list li {
+    border: 1px solid blue;
+    list-style: none;
+  }
+  ul {
+    padding: 0;
+  }
+  #message {
+    margin-top: 185px;
+    width: 500px;
+
+  }
+  </style>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+
+  $( function() {
+   $( "#sortable1, #sortable2" ).sortable({
+     connectWith: ".connectedSortable"
+   }).disableSelection();
+ } );
+
+</script>
+
+</head>
 <?php
 $server = "127.0.0.1";
 $username = "root";
@@ -67,13 +140,114 @@ for ($i=0; $i<sizeof($classesNeeded); $i++){
 	}
 
 }
-//echo "You are a: ".$year."<br><br>";
-//echo "You have taken:<br>";
+echo "You are a: ".$year."<br><br>";
+echo "You have taken:<br>";
 foreach ($classesTaken as $classes){
-	echo $classes;
+	echo $classes. "<br>";
 }
-//echo "<br>You still need to take:<br>";
+echo "<br>You still need to take:<br>";
 foreach ($classesNeeded as $classes){
-	echo $classes;
+	echo $classes. "<br>";
 }
 ?>
+<body>
+
+  <center><H1>Your 4 year Plan<H1></center>
+  <div id = "test">
+    <ul id="jsonData">
+        <li></li>
+    </ul>
+
+  </div>
+
+  <div id = "yearOne">
+    <center><H3>Year One<H3></center>
+
+      <ul id="sortable1" class="connectedSortable">
+        <li class="ui-state-default">PSYC100</li>
+        <li class="ui-state-default">INST126</li>
+        <li class="ui-state-default">STAT100</li>
+        <li class="ui-state-default">MATH115</li>
+        <li class="ui-state-default">INST126</li>
+      </ul>
+
+      <ul id="sortable2" class="connectedSortable">
+        <li class="ui-state-highlight">INST201</li>
+        <li class="ui-state-highlight">INST311</li>
+        <li class="ui-state-highlight">INST314</li>
+        <li class="ui-state-highlight">INST326</li>
+      </ul>`
+  </div>
+
+  <div id = "yearTwo">
+    <center><H3>Year Two<H3></center>
+    <ul id="sortable2" class="connectedSortable">
+      <li class="ui-state-default">INST327</li>
+      <li class="ui-state-default">INST335</li>
+      <li class="ui-state-default">INST346</li>
+      <li class="ui-state-default">INST352</li>
+    </ul>
+
+    <ul id="sortable2" class="connectedSortable">
+      <li class="ui-state-highlight">INST354</li>
+      <li class="ui-state-highlight">INST362</li>
+      <li class="ui-state-highlight">INST377</li>
+    </ul>`
+  </div>
+
+  <div id = "yearThree">
+    <center><H3>Year Three<H3></center>
+    <ul id="sortable2" class="connectedSortable">
+      <li class="ui-state-default">INST408B</li>
+      <li class="ui-state-default">INST414</li>
+    </ul>
+
+    <ul id="sortable2" class="connectedSortable">
+      <li class="ui-state-highlight">INST447</li>
+      <li class="ui-state-highlight">INST462</li>
+
+    </ul>`
+
+  </div>
+
+  <div id = "yearFour">
+    <center><H3>Year Four<H3></center>
+    <ul id="sortable2" class="connectedSortable">
+      <li class="ui-state-default">INST466</li>
+
+    </ul>
+
+    <ul id="sortable2" class="connectedSortable">
+      <li class="ui-state-highlight">INST490</li>
+
+    </ul>`
+  </div>
+<!--
+<div>
+  <div id="droppable" class="ui-widget-header">
+    <p>Drop data here. How much alcohol do you drink per week?</p>
+    <ul id="list">
+    </ul>
+  </div>
+
+  <div class="ui-widget-content draggable">
+    <p>19 years old, 2 times</p>
+  </div>
+  <div class="ui-widget-content draggable">
+    <p>23 years old, 5 times</p>
+  </div>
+  <div class="ui-widget-content draggable">
+    <p>30 years old, 4 times</p>
+  </div>
+  <div class="ui-widget-content draggable">
+    <p>15 years old, 0 times</p>
+  </div>
+</div>
+
+<div id="message" class="ui-widget-content ui-state-highlight">
+</div>
+-->
+
+
+</body>
+</html>
