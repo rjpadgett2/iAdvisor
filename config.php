cfa3e0d3b2ea5a80f5b8a4d0ee990529677f546c
Reggie Padgett <reggie.padgett2@gmail.com>
2018-11-10 23:24:25 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,22 @@
 <?php
 // For testing Purposes
-$server = "127.0.0.1";
-$username = "root";
-$password = "nilemonitor354";
+// $server = "127.0.0.1";
+// $username = "root";
+// $password = "nilemonitor354";
+// $db = "iAdvisor";
+// $port = "3306";
+
+// $connection = mysqli_connect($server, $username, $password, $db, $port);
+
+// // Check connection
+// if (!$connection) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+
+// For Cloud Purposes
+$server = "35.227.16.244";
+$username = "rpadgett";
+$password = "iadvisor";
 $db = "iAdvisor";
 $port = "3306";
 
@@ -12,20 +26,6 @@ $connection = mysqli_connect($server, $username, $password, $db, $port);
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-// For Cloud Purposes
-// $server = "35.227.16.244";
-// $username = "rpadgett";
-// $password = "iadvisor";
-// $db = "iAdvisor";
-// $port = "3306";
-//
-// $connection = mysqli_connect($server, $username, $password, $db, $port);
-//
-// // Check connection
-// if (!$connection) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
 
 
 ?>
