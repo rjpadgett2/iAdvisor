87002aa71e1f07fb9645392579676f52a2fa8ea0
Reggie Padgett <reggie.padgett2@gmail.com>
2019-01-11 20:52:55 -0500

    Fixed config file

--- connect/config.php
+++ connect/config.php
@@ -2,17 +2,17 @@
 session_start();
 Class dbObj{
   // For testing Purposes
-  var $server = "127.0.0.1";
-  var $username = "root";
-  var $password = "nilemonitor354";
-  var $db = "iAdvisor";
-  var $port = "3306";
-
-  // var $server = "104.196.186.97";
-  // var $username = "rpadgett";
-  // var $password = "iadvisor";
+  // var $server = "127.0.0.1";
+  // var $username = "root";
+  // var $password = "nilemonitor354";
   // var $db = "iAdvisor";
   // var $port = "3306";
+
+  var $server = "104.196.186.97";
+  var $username = "rpadgett";
+  var $password = "iadvisor";
+  var $db = "iAdvisor";
+  var $port = "3306";
   function getConnstring() {
 		$con = mysqli_connect($this->server, $this->username, $this->password, $this->db) or die("Connection failed: " . mysqli_connect_error());
 
